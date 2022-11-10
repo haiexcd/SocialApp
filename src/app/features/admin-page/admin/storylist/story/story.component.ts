3265ecb4f22c67018ce9dd923b184ca735e4d821
Hai <haiwinexcd@gmail.com>
2022-11-10 00:59:48 -0600

    added adminPage functions

---
+++ src/app/features/admin-page/admin/storylist/story/story.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NewsfeedStory } from 'src/app/shared/models/newsfeedStory';
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.sass']
+})
+export class StoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input() post? : NewsfeedStory
+
+}
